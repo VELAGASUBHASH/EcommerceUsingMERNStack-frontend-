9836653febb38ec00d3a2d3f5162b4c065b80d53
VELAGA SUBHASH <130762017+VELAGASUBHASH@users.noreply.github.com>
2025-08-29 09:20:53 +0530

    Update Axios.js

--- src/Library/Axios.js
+++ src/Library/Axios.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : "https://your-production-api.com"; 
-
 const axiosInstance = axios.create({
-  baseURL,
+  baseURL: "https://ecommerceusingmernstack-backend.onrender.com",
   withCredentials: true,
 });
 
